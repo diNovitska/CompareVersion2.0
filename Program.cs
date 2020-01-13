d7144b22d5a319bc555af53caae668ecef5907e1
diNovitska <di.novitska@gmail.com>
2020-01-13 12:01:47 +0200

    add VersionCompareText method(result in string format)

--- Program.cs
+++ Program.cs
@@ -9,18 +9,37 @@ namespace CompareVersion2._0
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(versionCompare("1.2", "1.2"));
-            Console.WriteLine(versionCompare("1.3", "1.2"));
-            Console.WriteLine(versionCompare("1.2", "1.5"));
-            Console.WriteLine(versionCompare("1.2.0", "1.2"));
-            Console.WriteLine(versionCompare("1.2", "1.2.0"));
-            Console.WriteLine(versionCompare("1.2.9.8.1", "1.3"));
-            Console.WriteLine(versionCompare("1.3", "1.2.9.8"));
-            Console.WriteLine(versionCompare("1.2.9", "1.2"));
-            Console.WriteLine(versionCompare("1.2", "1.2.9"));
+            Console.WriteLine(VersionCompareText(VersionCompare("1.2", "1.2")));
+            Console.WriteLine(VersionCompareText(VersionCompare("1.3", "1.2")));
+            Console.WriteLine(VersionCompareText(VersionCompare("1.2", "1.5")));
+            Console.WriteLine(VersionCompareText(VersionCompare("1.2.0", "1.2")));
+            Console.WriteLine(VersionCompareText(VersionCompare("1.2", "1.2.0")));
+            Console.WriteLine(VersionCompareText(VersionCompare("1.2.9.8.1", "1.3")));
+            Console.WriteLine(VersionCompareText(VersionCompare("1.3", "1.2.9.8")));
+            Console.WriteLine(VersionCompareText(VersionCompare("1.2.9", "1.2")));
+            Console.WriteLine(VersionCompareText(VersionCompare("1.2", "1.2.9")));
+
         }
 
-        public static int versionCompare(string str1, string str2)
+        public static string VersionCompareText(int result)
+        {
+            if (result == 0)
+            {
+                return "Versions are equal";
+            }
+            else
+            { if (result == 1)
+                {
+                    return "First version is newer";
+                }
+                else 
+            {
+                    return "Second version is newer";
+                }
+            }
+
+        }
+            public static int VersionCompare(string str1, string str2)
         {
             List<string> version1 = new List<string>((str1.Split('.')).ToList());
             List<string> version2 = new List<string>((str2.Split('.')).ToList());
